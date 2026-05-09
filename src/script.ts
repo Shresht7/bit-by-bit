a14dbd0c95c6dbe640557fdbb8d38be9b2243f0a
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-05-09 23:00:47 +0530

    Add `data-sync-group` attribute to bit cells for synchronized flipping

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,18 @@
 // Components
 import "./components/display-flex.ts";
 import "./components/bit-cell.ts";
+import type { BitCell } from "./components/bit-cell.ts";
+
+/** The collection of bit cells that are part of a sync group, allowing them to flip together when one is clicked. */
+const syncBits = document.querySelectorAll<BitCell>("section.showcase bit-cell[data-sync-group]");
+
+// Add click event listeners to each sync bit to flip all bits in the same sync group when one is clicked.
+syncBits.forEach(bit => bit?.addEventListener("click", () => {
+    const targetSyncGroup = bit.getAttribute("data-sync-group");
+    if (!targetSyncGroup) return;
+    syncBits.forEach(b => {
+        if (b.getAttribute("data-sync-group") === targetSyncGroup) {
+            b.value = b.value === 0 ? 1 : 0;
+        }
+    });
+}));
