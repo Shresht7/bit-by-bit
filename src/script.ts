da99722240ffb1c7761c9d374ff191377690dd93
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-05-10 15:30:50 +0530

    Enable synchronization of `<expanded-number>` values

--- src/script.ts
+++ src/script.ts
@@ -5,6 +5,7 @@ import "./components/expanded-number.ts";
 
 // Type Definitions
 import type { BitCell } from "./components/bit-cell.ts";
+import type { ExpandedNumber } from "./components/expanded-number.ts";
 
 /** The collection of bit cells that are part of a sync group, allowing them to flip together when one is clicked. */
 const syncBits = document.querySelectorAll<BitCell>("section.showcase bit-cell[data-sync-group]");
@@ -16,6 +17,20 @@ syncBits.forEach(bit => bit?.addEventListener("click", () => {
     syncBits.forEach(b => {
         if (b.getAttribute("data-sync-group") === targetSyncGroup) {
             b.value = b.value === 0 ? 1 : 0;
+        }
+    });
+}));
+
+/** The collection of expanded numbers that are part of a sync group, allowing them to update together when one is changed. */
+const syncNumbers = document.querySelectorAll<ExpandedNumber>("section.showcase expanded-number[data-sync-group]");
+
+// Add value-changed event listeners to each sync number to update all numbers in the same sync group when one is changed.
+syncNumbers.forEach(number => number?.addEventListener("value-changed", () => {
+    const targetSyncGroup = number.getAttribute("data-sync-group");
+    if (!targetSyncGroup) return;
+    syncNumbers.forEach(n => {
+        if (n.getAttribute("data-sync-group") === targetSyncGroup) {
+            n.value = number.value;
         }
     });
 }));
