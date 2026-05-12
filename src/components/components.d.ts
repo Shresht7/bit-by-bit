9ba57d14ae55d851af77dd013a1f44fe8b4dcf7d
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-05-12 15:32:05 +0530

    Remove stale references to `<display-flex>` in `components.d.ts`

--- src/components/components.d.ts
+++ src/components/components.d.ts
@@ -1,10 +1,8 @@
-import type { DisplayFlex } from "./display-flex";
 import type { BitCell } from "./bit-cell";
 import type { ExpandedNumber } from "./expanded-number";
 
 declare global {
     interface HTMLElementTagNameMap {
-        "display-flex": DisplayFlex;
         "bit-cell": BitCell;
         "expanded-number": ExpandedNumber;
     }
