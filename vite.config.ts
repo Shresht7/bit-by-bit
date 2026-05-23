6cd57ef3d31809eeb7678494cad5cdc5db4ef320
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-05-23 23:19:05 +0530

    Update Vite configuration to set base repository path

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import htmlPartials from "./plugin/html-partials";
 
+const GITHUB_REPOSITORY_NAME = "bit-by-bit";
+const GITHUB_REPOSITORY_BASE = `/${GITHUB_REPOSITORY_NAME}/`;
+
 export default {
+    base: GITHUB_REPOSITORY_BASE,
     plugins: [htmlPartials()]
 }
